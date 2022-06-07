12ad6a065aba11e98de020aeed945f7703257ac5
IbrahimSiddiqui007 <87603789+IbrahimSiddiqui007@users.noreply.github.com>
2022-06-07 21:27:36 +0400

    Update Assignment03_Procedural.cpp

--- Ibrahim7352396/Assignment03_Procedural/Assignment03_Procedural.cpp
+++ Ibrahim7352396/Assignment03_Procedural/Assignment03_Procedural.cpp
@@ -3,41 +3,69 @@
 #include <vector>
 #include <fstream>
 
-void print(std::vector<std::vector<char>> &vect);
+void print(std::vector<std::vector<char>>& Ladies,std::vector<std::vector<char>>& Men);
+void stopALadies(std::vector<std::vector<char>>& Ladies);
 
 int main()
 {
 
-	std::vector<std::vector<char>> bus
-	{
-		{' ','0','1','2','3'},
-		{' ',' ',' ',' ','D'},
-		{'1','E','E','E','E'},
-		{'2','E','E','E','E'},
-		{'3','E','E','E','E'},
-		{'4','E','E','E','E'},
-		{' ',' ',' ',' ','D'},
-		{'5','E','E','E','E'},
-		{'6','E','E','E','E'},
-		{'7','E','E','E','E'},
-		{'8','E','E','E','E'},
-		{' ',' ',' ',' ','D'}
-	};
+    std::vector<std::vector<char>> ladies
+    {
+        {' ','A','B','C','D'},
+        {' ',' ',' ','D','R'},
+        {'1','E','E','E','E'},
+        {'2','E','E','E','E'},
+        {'3','E','E','E','E'},
+        {'4','E','E','E','E'},
+    };
+    
+    std::vector<std::vector<char>> Men
+    {
+        {' ',' ',' ','D','R'},
+        {'5','E','E','E','E'},
+        {'6','E','E','E','E'},
+        {'7','E','E','E','E'},
+        {'8','E','E','E','E'},
+        {' ',' ',' ','D','R'}
+    };
 
-	print(bus);
+    
+
+    print(ladies,Men);
+    stopALadies(ladies);
+    print(ladies,Men);
 
 
 }
 
 
-void print(std::vector<std::vector<char>>& vect)
+void print(std::vector<std::vector<char>>& Ladies,std::vector<std::vector<char>>& Men)
 {
-	for (int row = 0; row < vect.size(); row++)
-	{
-		for (int col = 0; col < vect[row].size(); col++)
-		{
-			std::cout << vect[row][col];
-		}
-	}
+    
+    for (int row = 0; row < Ladies.size(); row++)
+    {
+        for (int col = 0; col < Ladies[row].size(); col++)
+        {
+
+            std::cout << Ladies[row][col] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    for (int row = 0; row < Men.size(); row++)
+    {
+        for (int col = 0; col < Men[row].size(); col++)
+        {
+
+            std::cout << Men[row][col] << " ";
+        }
+        std::cout << std::endl;
+    }
 }
+
+void stopALadies(std::vector<std::vector<char>>& Ladies)
+{
+    
+}
+
 
